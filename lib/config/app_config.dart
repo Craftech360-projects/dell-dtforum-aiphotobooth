9e890336cfa3dbb1f15d11ab0270bee2b40bdb2b
AbrahamCFT360 <abraham@craftech360.com>
2025-09-03 15:06:47 +0530

    Final Codebase.

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -4,7 +4,6 @@ class AppConfig {
   // Runpod Configuration
   static String get runpodApiKey => dotenv.env['RUNPOD_API_KEY'] ?? '';
   static String get runpodEndpointUrl => dotenv.env['RUNPOD_SWAPLAB_URL'] ?? '';
-  static String get comfyUIEndpointUrl => dotenv.env['RUNPOD_COMFYUI_URL'] ?? '';
   
   // Gemini API Configuration
   static String get geminiApiKey => dotenv.env['GEMINI_API_KEY'] ?? '';
